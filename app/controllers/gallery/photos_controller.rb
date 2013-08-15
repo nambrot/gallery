d7201f36c2a2a043e156b04378fff45545f18ea9
Nam Chu Hoai <nambrot@googlemail.com>
2013-08-15 23:39:33 +0200

    add page caching for photos json

--- app/controllers/gallery/photos_controller.rb
+++ app/controllers/gallery/photos_controller.rb
@@ -3,16 +3,17 @@ require_dependency "gallery/application_controller"
 module Gallery
   class PhotosController < ApplicationController
     respond_to :json
+    
+    if respond_to?('caches_action')
+      caches_action :index
+    end
 
     def index
       album = Album.find_by_id(params[:album_id])
       album = Album.find_by_uid_and_provider(params[:album_id].split('-').first, params[:album_id].split('-').last) unless album
       photos = album.photos
       if album.public
-        if stale? photos
-          respond_with photos, :only => [:id, :album_id, :source, :name, :aspect_ratio, :thumbnail]
-        end
-
+        respond_with photos, :only => [:id, :album_id, :source, :name, :aspect_ratio, :thumbnail]
       else
         respond_with []
       end
